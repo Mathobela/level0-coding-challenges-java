d371c04950568721be8e9d6a50dfb0468a8e4ace
Mathobela <mathobela.mawasha@outlook.com>
2021-11-12 01:32:55 +0200

    Task 0.10 solution

--- Task1.java
+++ Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
         int y = 1;
         System.out.println(x);
         System.out.println(y);
-        x += 3;
+        x = x + 3;
         y = y + x;
         System.out.println(x);
         System.out.print(y);
