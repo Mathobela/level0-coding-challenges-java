33138191ba0a7a2a373e7ad81bed5a38b50131e0
Mathobela <mathobela.mawasha@outlook.com>
2021-11-12 01:37:34 +0200

    Stored return value in a variable

--- Task5.java
+++ Task5.java
@@ -1,10 +1,9 @@
 public class Task5 {
     public static void main(String[] args) {
-        calculateArea(3, 6, 7);
+        double area = calculateArea(3, 6, 7);
     }
     public static double calculateArea(int length1, int length2, int length3) {
         double semiPeri = 0.5 * (length1 + length2 + length3);
-        double area = Math.sqrt(semiPeri * (semiPeri - length1) * (semiPeri - length2) * (semiPeri - length3));
-        return area;
+        return Math.sqrt(semiPeri * (semiPeri - length1) * (semiPeri - length2) * (semiPeri - length3));
     }
 }
