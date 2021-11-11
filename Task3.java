5fd50aa9a5de659f92550ca27f64cd5dcc649637
Mathobela <mathobela.mawasha@outlook.com>
2021-11-11 19:15:02 +0200

    Task 0.3 solution

---
+++ Task3.java
@@ -0,0 +1,8 @@
+public class Task3 {
+    public static void main(String[] args) {
+        hello("Paulina");
+    }
+     public static void hello(String name) {
+        System.out.print("Hello " + name + "!");
+    }
+}
