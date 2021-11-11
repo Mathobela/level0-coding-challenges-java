fc608189b9a8cb79858cc43ef2fddde4a2ca431e
Mathobela <mathobela.mawasha@outlook.com>
2021-11-11 19:25:12 +0200

    Task 0.4 solution

---
+++ Task4.java
@@ -0,0 +1,13 @@
+public class Task4 {
+    public static void main(String[] args) {
+        evenOrOdd(4);
+    }
+    public static void evenOrOdd(int number) {
+        if(number % 2 == 0) {
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
+}
