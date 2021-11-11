4558c5454e5d585e2c17ad29a3d69e09157649cf
Mathobela <mathobela.mawasha@outlook.com>
2021-11-11 18:32:11 +0200

    Task 0.2 solution

---
+++ Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 1;
+        System.out.println(x);
+        System.out.println(y);
+        x += 3;
+        y = y + x;
+        System.out.println(x);
+        System.out.print(y);
+    }
+}
