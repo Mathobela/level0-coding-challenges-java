f73e1fe97b7c5ea1350c94e8c2492b07c68d972c
Mathobela Mawasha <mathobela.mashiane@gmail.com>
2021-11-18 10:54:52 +0200

    Changed initial currentMax to be the first arg

--- Task6.java
+++ Task6.java
@@ -1,9 +1,10 @@
 public class Task6 {
     public static void main(String[] args) {
-        int maxNum = findMax(3, 22, 12, 40);
+        int maxNum = findMax(-3, -22, -12, -40);
     }
+
     public static int findMax(int ...numbers) {
-        int currentMax = 0;
+        int currentMax = numbers[0];
         for (int number : numbers) {
             if (number > currentMax) {
                 currentMax = number;
